9b9d3a5a98766020aea37e90715ab2592e22d121
Pablo Pernias <pablo@pernias.com>
2022-10-04 12:12:01 +0200

    clean up and improve a bit the code
    I think this can still be improved a bit to make it more readable and reduce the amount of reshapes that we do, but it's a bit better than before.

--- vivq.py
+++ vivq.py
@@ -5,44 +5,42 @@ from fast_pytorch_kmeans import KMeans
 
 
 class TemporalSpatialAttention(nn.Module):
-    def __init__(self, channels, size, frames, num_layers=4, num_heads=4, spatial_first=True):
+    def __init__(self, channels, size, frames, num_layers=4, num_heads=4, spatial_first=True, pos_encodings=True):
         super(TemporalSpatialAttention, self).__init__()
-        self.channels = channels
-        self.size = size
-        self.frames = frames
         self.spatial_first = spatial_first
-        self.transformer_layer = nn.TransformerEncoderLayer(d_model=channels, dim_feedforward=2048, nhead=num_heads, batch_first=True)
+        self.pos_encodings = pos_encodings
+        
+        self.transformer_layer = nn.TransformerEncoderLayer(d_model=channels, dim_feedforward=channels*4, nhead=num_heads, batch_first=True, norm_first=True, activation='gelu')
         self.spatial_transformer = nn.TransformerEncoder(encoder_layer=self.transformer_layer, num_layers=num_layers, norm=nn.LayerNorm(channels))
         self.temporal_transformer = nn.TransformerEncoder(encoder_layer=self.transformer_layer, num_layers=num_layers, norm=nn.LayerNorm(channels))
-
-    def forward(self, x):
-        """
-        x: 1 x 20 x 8 x 16 x 16
-        1. reshape & permute x: 1 * 20 x 16 x 8*8 = 20 x 8 x 256 -> 20 x 256 x 8
-        2. spatial attention: q k v -> 20 x 256 x 8 -> 20 x 256 x 8 * 20 x 8 x 256 = 1 x 256 x 256 -> 1 x 256 x 256 * 1 x 256 x 8
-        = 1 x 256 x 8
-        3. reshape & permute x: 1 * 16 * 16 x 20 x 8 = 256 x 20 x 8
-        4. temporal attention: q k v -> 256 x 20 x 8 -> 256 x 20 x 8 * 256 x 8 x 20 = 256 x 20 x 20 -> 256 x 20 x 20 * 256 x 20 x 8
-        = 256 x 20 x 8
-        :return: x: 1 x 20 x 8 x 16 x 16
-        """
-        bs, tz, _, _, _ = x.shape
+        
+        if pos_encodings:
+            self.spatial_positional_encoding = nn.Parameter(torch.randn(1, 1, size*size, channels) / channels ** 0.5)
+            self.temporal_positional_encoding = nn.Parameter(torch.randn(1, frames, 1, channels) / channels ** 0.5)
+    
+    def _spatial_attn(self, x, base_shape):
+        x = x.view(-1, *x.shape[2:]) # B x T x (H x W) x C -> (B x T) x (H x W) x C
+        x = self.spatial_transformer(x)
+        x = x.view(base_shape[0], base_shape[1], *x.shape[1:]) # (B x T) x (H x W) x C -> B x T x (H x W) x C
+        return x
+    
+    def _temporal_attn(self, x, base_shape):
+        mask = torch.triu(torch.ones(base_shape[1], base_shape[1]) * float('-inf'), diagonal=1)
+        x = x.permute(0, 2, 1, 3).view(-1, x.size(1), x.size(-1)) # B x T x (H x W) x C -> (B x H x W) x T x C
+        x = self.temporal_transformer(x, mask=mask)
+        x = x.view(base_shape[0], -1, *x.shape[2:]).permute(0, 2, 1, 3) # (B x H x W) x T x C -> B x T x (H x W) x C
+        return x
+    
+    def forward(self, x): 
+        base_shape = x.shape # x -> B x T x (H x W) x C
+        if self.pos_encodings:
+            x = x + self.spatial_positional_encoding + self.temporal_positional_encoding
         if self.spatial_first:
-            x = x.view(bs * tz, self.channels, self.size * self.size).permute(0, 2, 1)  # bs * tz x hz * wz x cz
-            x = self.spatial_transformer(x)
-            x = x.view(bs, tz, self.channels, self.size, self.size)
-            x = x.view(bs, tz, self.size*self.size, self.channels).permute(0, 2, 1, 3).view(bs*self.size*self.size, tz, self.channels)  # bs x hz * wz x tz * cz
-            mask = torch.tril(torch.ones(tz, tz, dtype=torch.float32)) * -10000
-            x = self.temporal_transformer(x, mask=mask)
-            x = x.view(bs, self.size, self.size, tz, self.channels).permute(0, 3, 4, 1, 2)
+            x = self._spatial_attn(x, base_shape)
+            x = self._temporal_attn(x, base_shape)
         else:
-            x = x.view(bs, tz, self.size*self.size, self.channels).permute(0, 2, 1, 3).view(bs*self.size*self.size, tz, self.channels)  # bs x hz * wz x tz * cz
-            mask = torch.tril(torch.ones(tz, tz, dtype=torch.float32)) * -10000
-            x = self.temporal_transformer(x, mask=mask)
-            x = x.view(bs, self.size, self.size, tz, self.channels).permute(0, 3, 4, 1, 2)
-            x = x.view(bs * tz, self.channels, self.size * self.size).permute(0, 2, 1)  # bs * tz x hz * wz x cz
-            x = self.spatial_transformer(x)
-            x = x.view(bs, tz, self.channels, self.size, self.size)
+            x = self._temporal_attn(x, base_shape)
+            x = self._spatial_attn(x, base_shape)
         return x
 
 
@@ -53,9 +51,9 @@ class Encoder(nn.Module):
         self.attention = TemporalSpatialAttention(hidden_channels, size, compressed_frames, num_layers=num_layers, num_heads=num_heads)
 
     def forward(self, x):
-        x = x.permute(0, 2, 1, 3, 4)
+        x = x.permute(0, 2, 1, 3, 4) # B x T x C x H x W -> B x C x T x H x W
         x = self.patch_emb(x)
-        x = x.permute(0, 2, 1, 3, 4)
+        x = x.view(*x.shape[:3], -1).permute(0, 2, 3, 1) # -> B x T x (H x W) x C [I'll use this as a reference shape, so every function returns this shape]
         x = self.attention(x)
         return x
 
@@ -63,12 +61,15 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, patch_size=(5, 8, 8), input_channels=3, hidden_channels=64, size=32, compressed_frames=5, num_layers=4, num_heads=4):
         super(Decoder, self).__init__()
-        self.patch_emb = nn.Conv3d(input_channels, hidden_channels, kernel_size=patch_size, stride=patch_size)
+        self.size = size
         self.attention = TemporalSpatialAttention(hidden_channels, size, compressed_frames, num_layers=num_layers, num_heads=num_heads, spatial_first=False)
+        self.unpatch_emb = nn.ConvTranspose3D(hidden_channels, input_channels, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
         x = self.attention(x)
-
+        x = x.permute(0, 3, 1, 2).view(x.size(0), x.size(3), x.size(1), sqrt(self.size(2)), sqrt(self.size(2))) # B x T x (H x W) x C -> B x C x T x H x W
+        x = self.unpatch_emb(x)
+        x = x.permute(0, 2, 1, 3, 4) # B x C x T x H x W -> B x T x C x H x W
         return x
 
 
@@ -111,15 +112,13 @@ class VQModel(nn.Module):
     def __init__(self, batch_size=1, compressed_frames=5, latent_size=32, c_hidden=64, c_codebook=16, codebook_size=1024,
                  num_layers_enc=4, num_layers_dec=4, num_heads=4):
         super().__init__()
-        self.batch_size = batch_size
-        self.latent_size = latent_size
-        self.c_hidden = c_hidden
         self.encoder = Encoder(hidden_channels=c_hidden, size=latent_size, compressed_frames=compressed_frames, num_layers=num_layers_enc, num_heads=num_heads)
         self.cod_mapper = nn.Sequential(
-            nn.Conv2d(c_hidden, c_codebook, kernel_size=1),
-            nn.BatchNorm2d(c_codebook),
+            nn.Linear(c_hidden, c_codebook),
+            nn.BatchNorm2d(c_codebook), # This should work because input has 4 dims, but not sure if it's properly used :/ 
         )
-        self.cod_unmapper = nn.Conv2d(c_codebook, c_hidden, kernel_size=1)
+        
+        self.cod_unmapper = nn.Linear(c_codebook, c_hidden)
         self.decoder = Decoder(hidden_channels=c_hidden, size=latent_size, compressed_frames=compressed_frames, num_layers=num_layers_dec, num_heads=num_heads)
 
         self.codebook_size = codebook_size
@@ -130,21 +129,18 @@ class VQModel(nn.Module):
         )
 
     def encode(self, x):
-        x = self.encoder(x)
-        bs, tz, cz, hz, wz = x.shape
-        x = x.view(bs*tz, cz, hz, wz)
+        x = self.encoder(x) # B x T x (H x W) x C
         x = self.cod_mapper(x)
-        qe, commit_loss, indices = self.vqmodule(x, dim=1)
+        qe, commit_loss, indices = self.vqmodule(x, dim=-1)
         return (x, qe), commit_loss, indices
 
     def decode(self, x):
         x = self.cod_unmapper(x)
-        x = x.view(self.batch_size, -1, self.c_hidden, self.latent_size, self.latent_size)
         x = self.decoder(x)
         return x
 
     def decode_indices(self, x):
-        return self.decode(self.vqmodule.vquantizer.idx2vq(x, dim=1))
+        return self.decode(self.vqmodule.vquantizer.idx2vq(x, dim=-1))
 
     def forward(self, x):
         print(x.shape)
